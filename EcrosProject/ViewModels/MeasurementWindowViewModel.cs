afa12191482ab4d88ed1f178fe367c11d322d791
3rdF4ce <61723217+3rdF4ce@users.noreply.github.com>
2021-03-19 21:44:58 +0300

    Добавлен OxiPlot

---
+++ EcrosProject/ViewModels/MeasurementWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.IO.Ports;
+using System.IO;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EcrosProject
+{
+    public class MeasurementWindowViewModel : INotifyPropertyChanged
+    {
+
+            
+        private int[] signal;
+        private string filename;
+       
+        public int[] Signal 
+        {
+            get { return signal; }
+            set {
+                 signal = value;
+                 OnPropertyChanged("Signal");
+                 }
+        
+        }
+              
+        public string Filename
+        {
+            get { return filename; }
+            set
+            {
+                filename = value;
+                OnPropertyChanged("Filename");
+             }
+
+        }
+
+
+        public MeasurementWindowViewModel()
+        {
+            Signal = new int[4400];
+            Filename = "Название файла";
+        }
+
+        private ICommand _showMeasurment;
+        public ICommand ShowMeasurement => _showMeasurment ?? (_showMeasurment = new RelayCommand(() =>
+                                                         {
+                                                             Readfile(@"\AK\Ecros\EcrosProjectWpf\EcrosProject\Data\data.pte");
+                                                         }));
+
+        public void Readfile(String readfilepath)
+        {
+
+            
+            StreamReader fstream = File.OpenText(readfilepath);
+            Filename = readfilepath;
+            int i = 0;
+            while ((readfilepath = fstream.ReadLine()) != null)
+            {
+                Signal[i] = Convert.ToInt32(readfilepath);
+                i++;
+            }
+            fstream.Close();
+        }
+       
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    
+    }
+    
+}
